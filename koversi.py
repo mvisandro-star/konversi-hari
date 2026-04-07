eb60cf281b821dc023ab3ec6e600c35a5af5573c
mvisandro-star <mvisandro@gmail.com>
2026-04-07 18:23:43 +0700

    tugas5.py

--- koversi.py
+++ koversi.py
@@ -1,11 +1,85 @@
-x = int(input("Masukkan jumlah hari: "))
+while True:
+    print("\n=== MENU OPERASI MATRIKS ===")
+    print("1. Penjumlahan")
+    print("2. Pengurangan")
+    print("3. Perkalian")
+    print("0. Exit")
 
-tahun = x // 365
-sisa = x % 365
+    pilih = input("Pilih menu: ")
 
-bulan = sisa // 30
-hari = sisa % 30
+    if pilih == "1":
+        A = [
+            [1, 2],
+            [3, 4]
+        ]
 
-print("Tahun:", tahun)
-print("Bulan:", bulan)
-print("Hari:", hari)
+        B = [
+            [5, 6],
+            [7, 8]
+        ]
+
+        C = []
+        for i in range(len(A)):
+            row = []
+            for j in range(len(A[0])):
+                row.append(A[i][j] + B[i][j])
+            C.append(row)
+
+        print("C = A + B")
+        for r in C:
+            print(r)
+
+    elif pilih == "2":
+        A = [
+            [1, 2],
+            [3, 4]
+        ]
+
+        B = [
+            [5, 6],
+            [7, 8]
+        ]
+
+        C = []
+        for i in range(len(A)):
+            row = []
+            for j in range(len(A[0])):
+                row.append(A[i][j] - B[i][j])
+            C.append(row)
+
+        print("C = A - B")
+        for r in C:
+            print(r)
+
+    elif pilih == "3":
+        A = [
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+
+        B = [
+            [7, 8],
+            [9, 10],
+            [11, 12]
+        ]
+
+        C = []
+        for i in range(len(A)):          # baris A
+            row = []
+            for j in range(len(B[0])):   # kolom B
+                total = 0
+                for k in range(len(A[0])):  # kolom A = baris B
+                    total += A[i][k] * B[k][j]
+                row.append(total)
+            C.append(row)
+
+        print("C = A * B")
+        for r in C:
+            print(r)
+
+    elif pilih == "0":
+        print("Program selesai.")
+        break
+
+    else:
+        print("Pilihan tidak valid!")
