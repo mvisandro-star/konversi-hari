76d9c13a0cae6b5c58fc170bf8e75353226e3c8e
mvisandro-star <mvisandro@gmail.com>
2026-04-17 20:39:10 +0700

    Update koversi.py

--- koversi.py
+++ koversi.py
@@ -1,85 +1,37 @@
+def fibonacci(n):
+    a, b = 1, 1
+    print("Barisan fibonacci sebanyak", n, "suku :")
+    for i in range(n):
+        print(a, end=", ")
+        a, b = b, a + b
+    print("\n")
+
+
+def perkalian(m, n):
+    hasil = m * n
+    print(f"Hasil dari {m} x {n} = {hasil}\n")
+
+
 while True:
-    print("\n=== MENU OPERASI MATRIKS ===")
-    print("1. Penjumlahan")
-    print("2. Pengurangan")
-    print("3. Perkalian")
-    print("0. Exit")
+    print("Menu Pilihan")
+    print("1. Barisan Fibonacci")
+    print("2. M Kali N")
+    print("0. Keluar")
 
-    pilih = input("Pilih menu: ")
+    pilihan = input("Pilih Menu: ")
 
-    if pilih == "1":
-        A = [
-            [1, 2],
-            [3, 4]
-        ]
+    if pilihan == "1":
+        jumlah = int(input("Masukkan jumlah suku: "))
+        fibonacci(jumlah)
 
-        B = [
-            [5, 6],
-            [7, 8]
-        ]
+    elif pilihan == "2":
+        m = int(input("Masukkan suatu bilangan bulat (M): "))
+        n = int(input("Masukkan suatu bilangan pengali (N): "))
+        perkalian(m, n)
 
-        C = []
-        for i in range(len(A)):
-            row = []
-            for j in range(len(A[0])):
-                row.append(A[i][j] + B[i][j])
-            C.append(row)
-
-        print("C = A + B")
-        for r in C:
-            print(r)
-
-    elif pilih == "2":
-        A = [
-            [1, 2],
-            [3, 4]
-        ]
-
-        B = [
-            [5, 6],
-            [7, 8]
-        ]
-
-        C = []
-        for i in range(len(A)):
-            row = []
-            for j in range(len(A[0])):
-                row.append(A[i][j] - B[i][j])
-            C.append(row)
-
-        print("C = A - B")
-        for r in C:
-            print(r)
-
-    elif pilih == "3":
-        A = [
-            [1, 2, 3],
-            [4, 5, 6]
-        ]
-
-        B = [
-            [7, 8],
-            [9, 10],
-            [11, 12]
-        ]
-
-        C = []
-        for i in range(len(A)):          # baris A
-            row = []
-            for j in range(len(B[0])):   # kolom B
-                total = 0
-                for k in range(len(A[0])):  # kolom A = baris B
-                    total += A[i][k] * B[k][j]
-                row.append(total)
-            C.append(row)
-
-        print("C = A * B")
-        for r in C:
-            print(r)
-
-    elif pilih == "0":
+    elif pilihan == "0":
         print("Program selesai.")
         break
 
     else:
-        print("Pilihan tidak valid!")
+        print("Pilihan tidak valid!\n")
